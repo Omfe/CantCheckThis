7c0c661f2269f17f4f7335b6c8dfd98a9468329e
Omar Gudino <omfegumo6@gmail.com>
2014-08-12 10:03:01 -0700

    Added /users as root view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :check_ins
 
   resources :users
+  
+  root :to => 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
